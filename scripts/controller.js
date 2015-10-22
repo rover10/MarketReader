ad69a3655c78015dee999c04c92121569ea5db06
rakesh <rakesh.r1303@gmail.com>
2015-10-23 01:12:35 +0530

    Adding updated files

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,8 +1,41 @@
 var app=angular.module('financialApp',[]);
 app.controller('finController', function($scope, $http){
     $scope.x = "Thimarket";
-    $scope.secClass = ["Equity", "Mutual funds", "Bond", "Commodities", "Forex", "Exchange Traded Funds", ];
+    $scope.secClass = ["Equities", "Mutual funds", "Bonds", "Commodities", "Forex", "Exchange Traded Funds", ];
     $scope.secTitle = $scope.secClass[0];
     $scope.fun = function(name){$scope.secTitle =name;};
     $scope.user = "Data";
 });
+
+
+app.directive('draggable', function($document) {
+    return function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0;
+      element.css({
+       position: 'relative',
+       
+      });
+      element.on('mousedown', function(event) {
+        // Prevent default dragging of selected content
+        event.preventDefault();
+        startX = event.screenX - x;
+        startY = event.screenY - y;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(event) {
+        y = event.screenY - startY;
+        x = event.screenX - startX;
+        element.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.off('mousemove', mousemove);
+        $document.off('mouseup', mouseup);
+      }
+    };
+  });
